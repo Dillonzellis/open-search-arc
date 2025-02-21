6c5f1cc42144843bd0695308c9f05d7ee00e2e03
Dillon Ellis <31318165+Dillonzellis@users.noreply.github.com>
2025-02-21 15:02:48 -0500

    ans example and new mappings for multisite queries

--- mappings.js
+++ mappings.js
@@ -1,47 +1,40 @@
-export const indexMappings = {
+export const Mappings = {
   mappings: {
+    dynamic: "strict",
     properties: {
+      _id: { type: "keyword" },
       display_date: {
         type: "date",
+        format: "yyyy-MM-dd'T'HH:mm:ss",
       },
-      type: {
-        type: "keyword",
-      },
-      subtype: {
-        type: "keyword",
-      },
+      canonical_website: { type: "keyword" },
       distributor: {
         properties: {
           reference_id: { type: "keyword" },
         },
       },
+      type: { type: "keyword" },
+      subtype: { type: "keyword" },
       taxonomy: {
         properties: {
           sections: {
             type: "nested",
             properties: {
               _id: { type: "keyword" },
-              // get website as well
             },
           },
           tags: {
+            type: "nested",
             properties: {
-              text: {
-                type: "text",
-                fields: {
-                  raw: { type: "keyword" },
-                },
-              },
+              text: { type: "keyword" },
               slug: { type: "keyword" },
             },
           },
         },
       },
+      websites: {
+        type: "flattened",
+      },
     },
   },
 };
-
-// canonical website(who created content)
-// store all ANS object except content_elements(for home page)
-// look into multi website
-// how to query to reteive object
